00fdecacc092af3878d0658144ce6e9b909fcee2
Khalid AlShumayri <53300785+Khalid-AlShumayri@users.noreply.github.com>
2024-07-20 11:47:38 +0300

    add estimated signal/noise subspace matrix

--- DoA_estimation.m
+++ DoA_estimation.m
@@ -7,13 +7,13 @@ clc
 % 2- Capon's Beamformer
 % 3- MUSIC algorithm
 
-M = 6;      % sensor
-K = 3;      % number of signals (sources)
-N = 10;     % number of observations
+M = 5;      % sensor
+K = 2;      % number of signals (sources)
+N = 40;     % number of observations
 d = 0.5;    % Distance between elements in wavelengths
-Pn = .1;    % Noise power
-sig_pr = [.9 .7 .5];    % signals' power
-DoA = [-30 20 35];
+Pn = .01;    % Noise power
+sig_pr = [.9 .4];    % signals' power
+DoA = [-30 10];
 iter = 1;
 j = 1;
 
@@ -23,6 +23,11 @@ angles=(-90:0.05:90);       % for grid search
 % far-field assumed
 a1 = exp(-1i*2*pi*d*(0:M-1)'*sin([angles(:).']*pi/180));  
 A  = generate_steering_matrix(M,d,DoA);
+S = diag(sqrt(sig_pr))*(randn(K,N)+1j*randn(K,N));
+Noise = sqrt(Pn)*(randn(M,N)+1j*randn(M,N));
+X = A*S + Noise;
+
+%% MUSIC Algorithm
 
 % comparing 3 different techniques
 spectrum_music = zeros(iter,length(angles));
@@ -30,17 +35,13 @@ spectrum_bf = zeros(iter,length(angles));
 spectrum_cap = zeros(iter,length(angles));
 
 
-S = diag(sqrt(sig_pr))*randn(K,N);
-Noise = sqrt(Pn)*randn(M,N);
-X = A*S + Noise;
 R = X*X'/N ;
-
 [Q ,D]  = eig(R);
 [D ,I]  = sort(diag(D),1,'descend');   %Find K largest eigenvalues
 D = diag(D);
 Q = Q (:,I);       % Sort the eigenvectors to put signal eigenvectors first 
 
-Qs = Q (:,K);       % Get the signal eigenvectors
+Qs = Q (:,1:K);       % Get the signal eigenvectors
 Qn = Q(:,K+1:M);    % Get the noise eigenvectors
 
 
@@ -49,6 +50,7 @@ for k=1:length(angles)
     search_music(k) = abs( 1/(a1(:,k)'*Qn*Qn'*a1(:,k)) ); 
     search_bf(k) = abs(a1(:,k)'*R*a1(:,k)/(a1(:,k)'*a1(:,k)));
     search_cap(k) = abs(20/( a1(:,k)'*inv(R)*a1(:,k) ));
+%     search_cap_cn = 
 end
 spectrum_music(j,:) = search_music;
 spectrum_bf(j,:)    = search_bf;
@@ -74,8 +76,27 @@ plot(angles,spectrum_cap,Color=[0.5, 0.2, 0.6]);
 xline(DoA,'-.',LineWidth=.9)
 
 legend('BF','Capon BF','Actual DoAs')
-title(['Spatial Spectrum : SNR = ',num2str(10*log10(max(sig_pr)/Pn)),'dB : Resolution = ',num2str(360/(3*M)),' degrees'])
+title(['Spatial Spectrum : SNR = ',num2str(10*log10(max(sig_pr)/Pn)),'dB : Resolution = ',num2str(360/(M)),' degrees'])
 xlabel('Angle in degrees')
 %% Analytical estimation
 
 A_est = X*S'*inv(S*S');      % estimate of the steering matrix
+P = S*S'/N;
+[Es,Ls] = eig(A*P*A');
+[Es,Ls] = nnzmat(Es,Ls);
+Rn = Noise*Noise'/N;
+[En,Ln] = eig(Rn);
+Ln = diag(Ln);
+Ln_hat = Ln(K+1:end);
+Ln_hat = diag(Ln_hat);
+En_hat = En(:,K+1:end);
+disp('R_hat = A*P*A'' + Cn')
+R_hat = A*P*A' + Rn;
+[Q_hat,D_hat] = eig(R_hat);
+
+D_hat = sort(diag(D_hat),1,'descend');
+% Ls = sort(diag(Ls),1,'descend')
+% Ln = sort(diag(Ln),1,'descend')
+% D = diag(D)
+disp(['norm(R) = ',num2str(norm(R,"fro"))])
+disp(['norm(R - R_hat) = ',num2str(norm(R-R_hat,"fro"))])
