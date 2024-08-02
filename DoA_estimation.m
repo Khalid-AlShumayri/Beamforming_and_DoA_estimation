a479cbf236040e6cbf6e2f8c09aa59b08de1dd38
Khalid AlShumayri <53300785+Khalid-AlShumayri@users.noreply.github.com>
2024-08-02 08:53:33 +0300

    modified the code Please enter the commit message for your changes. Lines starting

--- DoA_estimation.m
+++ DoA_estimation.m
@@ -2,74 +2,67 @@ close all
 clear all 
 clc 
 
-% Comparison between three grid-search approaches
+% Comparison between three grid-search DoA approaches
 % 1- Conventional Beamformer
 % 2- Capon's Beamformer
 % 3- spectrum-MUSIC algorithm
 
-M = 5;      % sensors
+M = 64;      % sensors
 K = 3;      % number of signals (sources)
-N = 10;     % number of observations
+N = 1;     % number of observations
 d = 0.5;    % Distance between elements in wavelengths
 Pn = .09;    % Noise power
 sig_pr = 0.9.*ones(1,K);    % signals' power
-DoA = -88 + 178*rand(1,K);
+DoA = [-30 20 35];
 Resolution = 2*180/pi/M;
 iter = 1;
 j = 1;
 
 % Compute steering Matrix and vectors
-angles=(-90:.5:90);       % for grid search
+angles=(-90:.1:90);       % for grid search
 
 % far-field assumption 
 a1 = exp(-1i*2*pi*d*(0:M-1)'*sin([angles(:).']*pi/180));  
 A  = generate_steering_matrix(M,d,DoA);
+
 S = diag(sqrt(sig_pr./2))*(randn(K,N)+1j*randn(K,N));
 
 Noise = sqrt(Pn/2)*( randn(M,N) + 1j*randn(M,N) );
 X = A*S + Noise;
-
-% generate coherent sources 
-correlation_matrix = toeplitz([1 .2 .1]);
-L = chol(correlation_matrix,"lower");
-S_corr = L*S;
-X_corr = A*S_corr + Noise;
-R_corr = X_corr*X_corr'./N;
-
+R = X*X'./N ;
 %% MUSIC Algorithm
 
-% comparing 3 different techniques
-spectrum_music = zeros(iter,length(angles));
-spectrum_bf = zeros(iter,length(angles));
-spectrum_cap = zeros(iter,length(angles));
+% comparing the three different techniques
+spec_music      = zeros(iter,length(angles));
+spec_music_coh = zeros(iter,length(angles));
+spec_bf = zeros(iter,length(angles));
+spec_cap = zeros(iter,length(angles));
 
-
-R = X*X'./N ;
+% Uncorrelated signals
 [Q ,D]  = eig(R);
 [D ,I]  = sort(diag(D),1,'descend');   %Find K largest eigenvalues
 D = diag(D);
 Q = Q (:,I);       % Sort the eigenvectors to put signal eigenvectors first 
-
 Qs = Q (:,1:K);       % Get the signal eigenvectors
 Qn = Q(:,K+1:M);    % Get the noise eigenvectors
 
-%% Grid search (or we can solve for the roots)
-for alpha=1:length(angles)
+
+%% Grid-search 
+for i=1:length(angles)
     % Compute MUSIC spectrum (spatial-spectrum)
-    search_music(alpha) = abs( 1/(a1(:,alpha)'*Qn*Qn'*a1(:,alpha)) ); 
-    search_bf(alpha) = abs(a1(:,alpha)'*R*a1(:,alpha)/(a1(:,alpha)'*a1(:,alpha)));
-    search_cap(alpha) = abs(20/( a1(:,alpha)'*inv(R)*a1(:,alpha) ));
+    srch_music(i)     = abs( 1/(a1(:,i)'*Qn*Qn'*a1(:,i)) ); 
+    srch_bf(i) = abs(a1(:,i)'*R*a1(:,i)/(a1(:,i)'*a1(:,i)));
+    srch_cap(i) = abs(1/( a1(:,i)'*inv(R)*a1(:,i) ));
 %     search_cap_cn = 
 end
-spectrum_music(j,:) = search_music;
-spectrum_bf(j,:)    = search_bf;
-spectrum_cap(j,:) = search_cap;
+spec_music(j,:)  = srch_music;
+spec_bf(j,:)    = srch_bf;
+spec_cap(j,:) = srch_cap;
 
 
-plot(angles,spectrum_music,Color=[0 .2 .9]); hold on; grid on;
-
-
+plot(angles,spec_music,Color=[0 .2 .9]); hold on; grid on;
 xline(DoA,'-.',LineWidth=.9)
+
 legend('MUSIC','Actual DoAs')
 title(['Spatial Spectrum : SNR = ',num2str(10*log10(max(sig_pr)/Pn)),'dB : Resolution = ',num2str(Resolution),' degrees'])
 xlabel('Angle [degrees]')
@@ -88,13 +81,13 @@ xlabel('Angle [degrees]')
 % title(['Spatial Spectrum : SNR = ',num2str(10*log10(max(sig_pr)/Pn)),'dB : Resolution = ',num2str(360/(M)),' degrees'])
 % xlabel('Angle in degrees')
 
-%% Peak detection and error
+% % Peak detection and error
 
 % Step 1: Initial peak detection
-[peaks, locs] = findpeaks(spectrum_music);
+[peaks, locs] = findpeaks(spec_music);
 
 % Step 2: Determine a dynamic threshold
-threshold = mean(spectrum_music);
+threshold = mean(spec_music) + 3*std(spec_music);
 
 % Step 3: Identify significant peaks
 AoA_peaks = peaks(peaks > threshold);
@@ -103,8 +96,8 @@ DoA = sort(DoA)
 RMSE = sqrt(sum((DoA-AoA_music).^2)/M)
 %% Analytical estimation (Validation?)
 
-A_est = X*S'*inv(S*S');      % estimate of the steering matrix
-
+% A_estm = X*S'*inv(S*S');      % estimate of the steering matrix
+% theta_estm = angle(A_estm(2,:));
 
 P = S*S'./N;
 [Us,Ls] = eig(A*P*A');
@@ -123,7 +116,6 @@ Un = Un(:,K+1:end);
 Ln = Ln(K+1:end);
 Ln = diag(Ln);
 
-disp('R_sum = A*P*A'' + Cn')
 R_hat = A*P*A' + Rn;
 [Q_sum,D_sum] = eig(R_hat);
 [D_sum,I_sum] = sort(diag(D_sum),1,'descend');
@@ -136,12 +128,3 @@ disp(['norm(R) = ',num2str(norm(R,"fro"))])
 disp(['norm(R - R_hat) = ',num2str(norm(R-R_hat,"fro"))])
 
 % AoA = asin( electric_angles./(d*2*pi) )*180/pi
-%% Root MUSIC (fft approach)
-
-spectrum_root = fftshift( sum( abs(fft(Qn,3*M)).^2 ,2) );
-% trial = fftshift( abs(fft(sum(Qn,2).^2,3*M)) );      % we have to take the fft of each column indiv. 
-[mini,I] = mink(spectrum_root,K);
-electric_angles = (I-1).*2*pi/M;
-% figure
-% plot(1./spectrum_root); hold on; grid on;
-% plot(1./trial); 
